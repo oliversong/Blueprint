5a18268afb77d859a31a9d965cb6f8785ace3dd9
Oliver Song <osong@mit.edu>
2014-02-22 02:13:21 -0500

    Make easier

--- client/views/pages/landing.js
+++ client/views/pages/landing.js
@@ -1,11 +1,15 @@
-Template.landing.greeting = function () {
-  return "Welcome to Blueprint.";
-};
+Template.landing.helpers({
+  greeting: function () {
+    return "Welcome to Blueprint.";
+  },
+  // add more helpers here
+});
 
 Template.landing.events({
   'click input' : function () {
     // template data, if any, is available in 'this'
     if (typeof console !== 'undefined')
       console.log("You pressed the button");
-  }
+  },
+  // add more events here
 });
